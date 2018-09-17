d54a1c6ec049a42a7a29f5e7186baef3c85b7669
Govind Dandekar <govind@Govinds-MacBook-Air.local>
2018-09-16 23:10:28 -0700

    Updated port code

--- server/server.js
+++ server/server.js
@@ -4,12 +4,11 @@ const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 });
 //configure express status middleware to serve up the public folder
