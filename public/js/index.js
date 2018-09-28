e5e3f956d0e896dd4216041bcfe2753b1df19050
Govind Dandekar <govind@Govinds-MacBook-Air.local>
2018-09-28 14:23:25 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,22 @@
 var socket = io(); //initiating request to open socket and keep open
 //variable required to listen
 
+function scrollToBottom () {
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight(); //prev moves to previous child
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);//move to bottom of messages area
+  }
+} //run every time we send a new message
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -21,6 +37,7 @@ socket.on('newMessage', function(message) {
      createdAt: formattedTime
    });
    jQuery('#messages').append(html);
+   scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -33,6 +50,7 @@ socket.on('newLocationMessage', function(message){
   })
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function(e){
